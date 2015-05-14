f43e1fd8458320f38ae7952f26d94ac1a43fc1f9
tgoldenberg <thomasagoldenberg@gmail.com>
2015-05-14 17:54:31 -0400

    style request form

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -70,6 +70,6 @@ class RequestsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def request_params
-      params.require(:request).permit(:body, :sender_id, :recipient_id, :seen)
+      params.require(:request).permit(:body, :sender_id, :recipient_id, :seen, :time, :stripe)
     end
 end
